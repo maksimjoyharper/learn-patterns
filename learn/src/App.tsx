03e6c4eb0c6feff5e9e97352d88e6833ae797598
maksimjoyharper <makksss1123@icloud.com>
2024-03-18 19:43:51 +0300

    lesson 1

---
+++ learn/src/App.tsx
@@ -0,0 +1,7 @@
+import { AbstructFactory } from "./patterns/factory/abstructFactory";
+// import { factory } from "./patterns/factory/factory";
+
+export const App = () => {
+  AbstructFactory();
+  return <></>;
+};
